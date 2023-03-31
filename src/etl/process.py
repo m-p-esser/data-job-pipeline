5e5d016497bc6c56c91ef1322f59a6ab953e17bc
Marc-Philipp Esser <m-esser@mail.de>
2023-03-31 23:32:51 +0200

    Added flatten json helper function

--- src/etl/process.py
+++ src/etl/process.py
@@ -1,73 +1,20 @@
 """Python module related to process data"""
 
-import json
+def flatten_json(y):
+    """ Flatten a dict (so it can be stored as JSONL file)"""
+    out = {}
 
-def extract_search_metadata(response_dict: dict) -> dict:
-   """Extract the metadata from the response_dict
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[name[:-1]] = x
 
-   Parameters
-   ----------
-   response_dict : dict
-      The response_dict to extract the metadata from
-   """
-
-   search_metadata = response_dict["search_metadata"]
-
-   return search_metadata
-
-def extract_search_parameters(response_dict: dict) -> dict:
-   """Extract the parameters from the response_dict
-
-   Parameters
-   ----------
-   response_dict : dict
-      The response_dict to extract the parameters from
-   """
-
-   search_parameters = response_dict["search_parameters"]
-
-   return search_parameters
-
-def extract_job_results(response_dict: dict) -> dict:
-   """Extract the job results from the response_dict
-
-   Parameters
-   ----------
-   response_dict : dict
-      The response_dict to extract the job results from
-   """
-
-   job_results = response_dict["jobs_results"]
-
-   return job_results
-
-def prepare_data_for_bigquery(response_dict: dict) -> dict:
-   """Process the data for BigQuery data ingestion
-   
-   Parameters
-   ----------
-   response_dict : dict
-      The response_dict to process
-   
-   """
-
-   search_metadata = extract_search_metadata(response_dict)
-   search_parameters = extract_search_parameters(response_dict)
-   job_results = extract_job_results(response_dict)
-
-   prepared_data = {
-      u"search_metadata": {
-        "id": "64201eb234ff954f9eb2f476",
-        "status": "Success"
-    },
-      u"search_parameters": {
-        "q": "Data Analyst",
-        "engine": "google_jobs"
-    },
-      u"job_results": {
-         "title": "Data Analyst (m/w/d)",
-         "company_name": "F mal s GmbH",
-        }
-   }
-
-   return prepared_data
+    flatten(y)
+    return out
