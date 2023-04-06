9e0dbc7317fbc0f42ffeaef0a46f39df6471f02b
Marc-Philipp Esser <m-esser@mail.de>
2023-04-06 21:00:02 +0200

    Add module to process data

--- src/etl/process.py
+++ src/etl/process.py
@@ -1,20 +1,37 @@
-"""Python module related to process data"""
+"""Python module to process data"""
+
+from gensim.utils import tokenize
+
 
 def flatten_json(y):
     """ Flatten a dict (so it can be stored as JSONL file)"""
     out = {}
 
-    def flatten(x, name=''):
+    def flatten(x, name=""):
         if type(x) is dict:
             for a in x:
-                flatten(x[a], name + a + '_')
+                flatten(x[a], name + a + "_")
         elif type(x) is list:
             i = 0
             for a in x:
-                flatten(a, name + str(i) + '_')
+                flatten(a, name + str(i) + "_")
                 i += 1
         else:
             out[name[:-1]] = x
 
     flatten(y)
     return out
+
+
+def tokenize_text(text: str) -> list[str]:
+    """Tokenize text
+
+    Parameters
+    ----------
+    text : str
+          Text to tokenize
+    """
+
+    tokens = list(tokenize(text, deacc=True, lower=True))
+
+    return tokens
